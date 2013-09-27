74504b57bbee999a4359985066a2b7c1bfb31bed
David P Henderson <dphenderson2@gmail.com>
2013-09-27 05:56:02 -0700

    Modified README and iPhoto2Web.rb to reflect the version of iPhoto with which this project will work.

--- README.md
+++ README.md
@@ -6,6 +6,9 @@ Rather than copying the images and thumbnails from the iPhoto Library,
 iPhoto2Web creates symlinks to the actual files referenced in the iPhoto Libray
 database.
 
+**NOTE:** Currently, this program is only useful with iPhoto ‘09 (version 8.1.2 (424)) as
+the structure and items within the library package often change between
+versions.
 
 iPhoto is a registered trademark of Apple Inc.
 
--- iPhoto2Web.rb
+++ iPhoto2Web.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 #
-# This program made possible with help from prior research on the iPhoto database from the following web pages —
+# This program made possible with help from prior research on the iPhoto(R) ‘09 version 8.1.2 database from the following web pages —
 # 	http://gskluzacek.tumblr.com/post/9111450792/more-on-iphotomain-db-iphotoaux-db-date-formats
 # 	http://gskluzacek.tumblr.com/post/9080035545/iphotomain-db-table-relationships
 # 	http://gskluzacek.tumblr.com/post/243137142/finding-the-original-photo-file-in-the-iphoto-library
@@ -13,7 +13,7 @@ require 'sqlite3'
 require "optparse"
 
 # set :path to the default path to the iPhoto Library
-options = {:path => File.join(Dir.home, 'Pictures'), :by_event => true}
+options = {:path => File.join(Dir.home, 'Pictures'), :by_event => true, :by_date => true}
 
 OptionParser.new do |opts|
 
@@ -22,11 +22,20 @@ OptionParser.new do |opts|
 	opts.banner = "#{prog_name} opens and queries the iPhoto Library.\n\n"\
 		"Usage: #{prog_name} [options] [file]"
 
-	opts.on( "-a", "--album [AlbumName]", String,
-	         "Create layout from user albums or a named album") do |name|
+	opts.on( "-a", "--album [AlbumName]", String, "Create layout from user albums or a named album.") do |name|
 		options[:by_album] = true
 		options[:by_event] = false
 		options[:album_name] = name || ''
+	end
+
+	opts.on( "-D", "--Date", String,
+	         "Create layout from an event date") do |date|
+		options[:the_date] = date || ''
+	end
+
+	opts.on( "-E", "--event-name [EventName]", String,
+	         "Create layout from events (default) or a named event.") do |name|
+		options[:the_name] = name || ''
 	end
 
 	opts.on("-p", "--path PATH", "Path to iPhoto Library being opened, default: #{options[:path]}.") do |p|
@@ -56,11 +65,13 @@ abort("#{main_f} doesn't exist.") unless File.exists?(main_f)
 # select primaryKey, datetime(photoDate + julianday('2000-01-01 00:00:00')) as photoDate, archiveFilename from SqPhotoInfo order by archiveFilename desc;
 #------======-------#
 
+# " DATETIME(e.rollDate + JULIANDAY('2000-01-01 00:00:00')) AS eventDate,"\
+
 main_db = SQLite3::Database.new(main_f)
 columns, *rows = main_db.execute2("SELECT"\
 								  " e.primaryKey AS event,"\
 								  " e.name AS eventName,"\
-								  " DATETIME(e.rollDate + JULIANDAY('2000-01-01 00:00:00')) AS eventDate,"\
+								  " e.rollDate AS eventDate,"\
 								  " pi.primaryKey AS fotoInfo,"\
 								  " pi.archiveFilename AS fotoName,"\
 								  " DATETIME(pi.photoDate + JULIANDAY('2000-01-01 00:00:00')) AS fotoDate,"\
