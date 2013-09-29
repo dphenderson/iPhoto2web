29b5de06b3fd65534689ac1c740c6feb7b6161c7
David P Henderson <dphenderson2@gmail.com>
2013-09-28 18:26:54 -0700

    Added version information for version of iPhoto supported. Added Apple Inc trademark recognition.

--- query_statements.sql
+++ query_statements.sql
@@ -1,6 +1,9 @@
 /*
  * This file serves as a proving ground for testing relationships between the
- * tables contained in iPhotoMain.db.
+ * tables contained in iPhotoMain.db contained within the iPhoto(R) Library
+ * package of iPhoto '09 version 8.1.4 (424).
+ *
+ * iPhoto is a registered trademark of Apple Inc.
  */
 
 -- keyword relationship
@@ -9,6 +12,7 @@ kw.title AS keyword
 FROM KeywordsPhotosJoin AS kpj
 JOIN SqPhotoInfo AS pi ON pi.primaryKey=kpj.sqPhotoInfo
 JOIN SqKeyword AS kw ON kw.primaryKey=kpj.sqKeyword;
+
 -- event relationship
 SELECT e.primaryKey AS event,
 e.name AS eventName,
