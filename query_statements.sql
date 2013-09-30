7f0ec782ad6f20ca116b8a19dabd0fa727feda26
David P Henderson <dphenderson2@gmail.com>
2013-09-29 21:17:39 -0700

    Added demostration of relationship of tables: SqPhotoInfo, SqFileImage, and SqFileInfo. Added explanation of SqFileImage.imageType.

--- query_statements.sql
+++ query_statements.sql
@@ -6,6 +6,28 @@
  * iPhoto is a registered trademark of Apple Inc.
  */
 
+/* SqFileImage.imageType is one of the following
+ *
+ * type=8: the original image if it is a raw data image
+ * type=7: key photo
+ * type=6: the image itself when you double click on the thumbnail
+ * type=5: thumbnail
+ * type=1: the original if it is modified
+ *
+ * 8, 6, 5, and 1 have physical representations on disk.
+ */
+
+-- photo relationship
+SELECT pi.primaryKey AS photoID,
+pi.archiveFilename AS photoName,
+DATETIME(pi.photoDate + JULIANDAY('2000-01-01 00:00:00')) AS photoDate,
+fim.imageType AS photoType,
+fi.relativePath AS photoPath
+FROM SqPhotoInfo AS pi
+JOIN SqFileImage AS fim ON fim.photoKey = pi.primaryKey
+JOIN SqFileInfo AS fi ON fi.primaryKey = fim.sqFileInfo
+ORDER BY photoType;
+
 -- keyword relationship
 SELECT pi.archiveFilename AS fotoName,
 kw.title AS keyword
@@ -30,6 +52,6 @@ JOIN SqPhotoInfo AS pi ON pi.event = e.primaryKey
 JOIN SqFileImage AS fim ON fim.photoKey = pi.primaryKey
 JOIN SqFileInfo AS fi ON fim.sqFileInfo = fi.primaryKey
 WHERE fim.imageType = 6
-ORDER BY eventDate ASC, fotoname ASC, imageType ASC
+ORDER BY eventDate ASC, fotOName ASC, imageType ASC;
 
--- dbext:profile=tati
+-- dbext:profile=iphoto2web
