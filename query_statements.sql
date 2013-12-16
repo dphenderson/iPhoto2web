c7c78f7fb7a4633975a1d6ac1c93149393682343
David P Henderson <dphenderson2@gmail.com>
2013-12-16 05:44:12 -0800

    Added information relating to date storage and retrieval in iPhotoMain.db. Corrected grammatical error in a comment.

--- query_statements.sql
+++ query_statements.sql
@@ -17,8 +17,23 @@
  * 8, 6, 5, and 1 have physical representations on disk.
  */
 
+/*
+ * Dates in iPhotoMain.db are expressed as the numer of fractional days elapsed
+ * since 2000-01-01 00:00:00 local time for example a photo taken on 7 Sept
+ * 2013 at 11:27:58 a.m. will be stored as 4998.47775462963 that is 4998 days
+ * and about 48% of a day. 
+ *
+ * The easiest means of expressing the date in terms more familiar to humans is
+ * to use the julianday function in sqlite3 passing 2000-01-01 00:00:00 as the
+ * argument and then add the iPhoto date representation to that value and pass
+ * that sum as the argument to the sqlite3 datetime function.
+ *
+ * The above information came from
+ * <http://gskluzacek.tumblr.com/post/9111450792/more-on-iphotomain-db-iphotoaux-db-date-formats>
+ */
+
 -- photo relationship
--- information about individual photos come from 3 tables:
+-- information about individual photos comes from 3 tables:
 -- SqPhotoInfo, SqFileImage, and SqFileInfo
 SELECT pi.primaryKey AS photoID,
 pi.archiveFilename AS photoName,
@@ -28,7 +43,7 @@ fi.relativePath AS photoPath
 FROM SqPhotoInfo AS pi
 JOIN SqFileImage AS fim ON fim.photoKey = pi.primaryKey
 JOIN SqFileInfo AS fi ON fi.primaryKey = fim.sqFileInfo
-ORDER BY photoType;
+ORDER BY fim.imageType;
 
 -- album relationship
 SELECT al.name AS albumName,
@@ -44,8 +59,8 @@ JOIN SqAlbum AS al ON al.primaryKey = apj.sqAlbum
 JOIN SqPhotoInfo AS pi on pi.primaryKey = apj.sqPhotoInfo
 JOIN SqFileImage AS fim ON fim.photoKey = pi.primaryKey
 JOIN SqFileInfo AS fi ON fi.primaryKey = fim.sqFileInfo
-WHERE albumType = 'Album'								-- user generated albums
-AND albumUID < 999000									-- Apple generated albums are >= 999000
+WHERE albumType = 'Album'												-- user generated albums
+AND albumUID < 999000													-- Apple generated albums are >= 999000
 ORDER BY albumName, apj.photosOrder;
 
 -- keyword relationship
